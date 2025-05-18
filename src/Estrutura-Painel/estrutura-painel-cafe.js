9b40dc382e2644e6dc0d2600e62c2b52fe2c6008
Lucas Dias Developer <devweblucasdias@gmail.com>
2025-05-18 20:57:42 -0300

    pagina incial montada

--- src/Estrutura-Painel/estrutura-painel-cafe.js
+++ src/Estrutura-Painel/estrutura-painel-cafe.js
@@ -1,116 +1,66 @@
 import React from "react";
 import logo from "../Assets/logo-IA-posto-sao-paulo-3.png";
-import cafeCurto from "../Assets/cafe-curto.png";
-import cafeNormal from "../Assets/cafe-normal.png";
-import leiteComAchocolatado from "../Assets/leite-achocolatado.png";
 import "./estrutura-painel-cafe.css";
 
-// Dados organizados
-const coluna1 = [
-  { nome: "Café Normal(Expresso)", preco: "4,75", imagem: cafeCurto },
-  { nome: "Café Curto(Expresso-Curto)", preco: "4,50", imagem: cafeNormal },
-  { nome: "Capuccino", preco: "6,00", imagem: cafeNormal },
-];
+// Imagens dos cafés
+import cafeNormal from "../Assets/cafe-normal.png";
+import cafeCurto from "../Assets/cafe-normal.png";
+import capuccino from "../Assets/cafe-normal.png";
+import mocaccino from "../Assets/cafe-normal.png";
+import italiana from "../Assets/cafe-normal.png";
+import chocolate from "../Assets/cafe-normal.png";
+import chantilly from "../Assets/cafe-normal.png";
+import cremeAvela from "../Assets/cafe-normal.png";
 
-const coluna2 = [
-  { nome: "Mocaccino", preco: "6,00", imagem: cafeNormal },
-  { nome: "Italiana(Pingado)", preco: "6,00", imagem: cafeNormal },
-  { nome: "Chocolate Quente", preco: "6,00", imagem: cafeNormal },
+const cafes = [
+  { nome: "Café Normal (Expresso)", preco: "4,75", imagem: cafeNormal },
+  { nome: "Café Curto (Expresso Curto)", preco: "4,50", imagem: cafeCurto },
+  { nome: "Capuccino", preco: "6,00", imagem: capuccino },
+  { nome: "Mocaccino", preco: "6,00", imagem: mocaccino },
+  { nome: "Italiana (Pingado)", preco: "6,00", imagem: italiana },
+  { nome: "Chocolate Quente", preco: "6,00", imagem: chocolate },
 ];
 
 const adicionais = [
-  { nome: "Adicional Chantilly", preco: "5,30", imagem: cafeNormal },
-  { nome: "Adicional Creme de Avelã", preco: "6,00", imagem: cafeNormal },
+  { nome: "Chantilly", preco: "5,30", imagem: chantilly },
+  { nome: "Creme de Avelã", preco: "6,00", imagem: cremeAvela },
 ];
 
-const leite = [
-  {
-    nome: "Leite com Achocolatado",
-    preco: "7,50",
-    imagem: leiteComAchocolatado,
-  },
-];
-
-const PainelPostoSaoPauloCafe = () => {
+const PainelCafeCards = () => {
   return (
     <div className="container">
       <div className="imagens-banner">
         <img src={logo} alt="Logo" className="logo-banner" />
       </div>
-
       <h2 className="coluna-titulo">Cafés & Leite</h2>
 
-      <div className="duas-colunas">
-        <div className="coluna">
-          {coluna1.map((item, index) => (
-            <div key={index} className="item">
-              <img
-                src={item.imagem}
-                alt={item.nome}
-                className="cafe-icon-round"
-              />
-              <span className="name">{item.nome}</span>
-              <span className="dots"></span>
-              <span className="price">{item.preco}</span>
-            </div>
-          ))}
-        </div>
-
-        <div className="coluna">
-          {coluna2.map((item, index) => (
-            <div key={index} className="item">
-              <img
-                src={item.imagem}
-                alt={item.nome}
-                className="cafe-icon-round"
-              />
-              <span className="name">{item.nome}</span>
-              <span className="dots"></span>
-              <span className="price">{item.preco}</span>
-            </div>
-          ))}
-        </div>
+      <div className="cards-grid">
+        {cafes.map((item, index) => (
+          <div key={index} className="card">
+            <img src={item.imagem} alt={item.nome} className="card-img-large" />
+            <span className="card-title">{item.nome}</span>
+            <span className="card-price">R$ {item.preco}</span>
+          </div>
+        ))}
       </div>
 
-      <div className="item-separador" />
-
-      <div className="coluna">
-        <div className="bloco-subtitulos">
-          <div className="bloco">
-            <h3 className="subtitulo">Adicionais</h3>
-            {adicionais.map((item, index) => (
-              <div key={index} className="item-subtitle">
-                <img
-                  src={item.imagem}
-                  alt={item.nome}
-                  className="cafe-icon-round"
-                />
-                <span className="name-subtitle">{item.nome}</span>
-                <span className="dots"></span>
-                <span className="price">{item.preco}</span>
-              </div>
-            ))}
+      <h3 className="subtitulo">Adicionais</h3>
+      <div className="linha-horizontal">
+        {adicionais.map((item, index) => (
+          <div key={index} className="item item-inline">
+            <img
+              src={item.imagem}
+              alt={item.nome}
+              className="cafe-icon-round"
+            />
+            <span className="name">Adicional {item.nome}</span>
+            <span className="dots"></span>
+            <span className="price">{item.preco}</span>
           </div>
-
-          <div className="bloco">
-            <h3 className="subtitulo">Leite</h3>
-            {leite.map((item, index) => (
-              <div key={index} className="item-subtitle">
-                <img
-                  src={item.imagem}
-                  alt={item.nome}
-                  className="cafe-icon-round"
-                />
-                <span className="name-subtitle">{item.nome}</span>
-                <span className="dots"></span>
-                <span className="price">{item.preco}</span>
-              </div>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 };
 
-export default PainelPostoSaoPauloCafe;
+export default PainelCafeCards;
